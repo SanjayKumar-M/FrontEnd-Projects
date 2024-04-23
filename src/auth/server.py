39f0245dfaf2aaba167b96fb052ec75856cdfd41
SanjayKumar-M <sanjaymoad786@gmail.com>
2024-04-23 11:13:59 +0530

    kubenertes pod deployment

--- src/auth/server.py
+++ src/auth/server.py
@@ -7,7 +7,10 @@ db = MySQL(server)
 
 #configurations for mysql
 server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
-
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = int(os.environ.get("MYSQL_PORT"))
 
 @server.route("/login", methods=["POST"])
 def login():
@@ -29,3 +32,29 @@ def login():
     else:
         return "invalid credentials",401
     
+@server.route("/validate",methods=["POST"])
+
+def validate():
+    encoded_jwt = request.headers["Authorization"]
+    if not encoded_jwt:
+        return "missing credentials",401
+    
+    encoded_jwt = encoded_jwt.split(" ")[1]
+    
+    try:
+        decoded = jwt.decode(encoded_jwt,os.environ.get("JWT_SECRET"),algorithm=["HS256"])
+    except:
+        return "not authorized", 403    
+    return decoded,200
+    
+def createJWT(username,secret,authz):
+    return jwt.encode({
+        "username":username,
+        "exp":datetime.datetime.now(tz=datetime.timezone.utc)+ datetime.timedelta(days=1),
+        "iat":datetime.datetime.utcnow(),
+        "admin":authz,
+    },secret,algorithm="HS256")
+    
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0",port=5000)
