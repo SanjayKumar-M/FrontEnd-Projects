5e10b6eebea071dc5934a56fc1b94a501b81b809
SanjayKumar-M <sanjaymoad786@gmail.com>
2024-04-24 23:53:15 +0530

    rabbitmq configuration updated

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -5,8 +5,10 @@ from auth import validate
 from auth_svc import access 
 from storage import util
 server = Flask(__name__)
+server.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/videos"
+
 mongo_video = PyMongo(server, uri="mongodb://host.minikube.internal:27017/videos")
-mongo_mp3 = PyMongo(server, uri="mongodb://host.minikube.internal:27017/mp3s")
+# mongo_mp3 = PyMongo(server, uri="mongodb://host.minikube.internal:27017/mp3s")
 mongo = PyMongo(server)
 fs = gridfs.GridFS(mongo.db)
 conection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
@@ -48,27 +50,28 @@ def upload():
     
 @server.route("/download", methods=["GET"])    
 def download():
-    access, err = validate.token(request)
+    pass
+    # access, err = validate.token(request)
 
-    if err:
-        return err
+    # if err:
+    #     return err
 
-    access = json.loads(access)
+    # access = json.loads(access)
 
-    if access["admin"]:
-        fid_string = request.args.get("fid")
+    # if access["admin"]:
+    #     fid_string = request.args.get("fid")
 
-        if not fid_string:
-            return "fid is required", 400
+    #     if not fid_string:
+    #         return "fid is required", 400
 
-        try:
-            out = fs_mp3s.get(ObjectId(fid_string))
-            return send_file(out, download_name=f"{fid_string}.mp3")
-        except Exception as err:
-            print(err)
-            return "internal server error", 500
+    #     try:
+    #         out = fs_mp3s.get(ObjectId(fid_string))
+    #         return send_file(out, download_name=f"{fid_string}.mp3")
+    #     except Exception as err:
+    #         print(err)
+    #         return "internal server error", 500
 
-    return "not authorized", 401
+    # return "not authorized", 401
 
 
 if __name__ == "__main__":
